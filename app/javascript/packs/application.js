76d66a547eeb59e0079083c062399921cd0ad586
claire demont <claire.edith.demont@gmail.com>
2019-02-19 16:09:06 +0100

    card de la show ok

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,15 @@
 import "bootstrap";
+const flatpickr = require("flatpickr");
+
+// ./packs/application.js
+import { Application } from "stimulus";
+// import Flatpickr
+import Flatpickr from "stimulus-flatpickr";
+
+import { definitionsFromContext } from "stimulus/webpack-helpers";
+const application = Application.start();
+const context = require.context("../controllers", true, /\.js$/);
+application.load(definitionsFromContext(context));
+
+// Manually register Flatpickr as a stimulus controller
+application.register("flatpickr", Flatpickr);
