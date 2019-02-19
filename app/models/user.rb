f949e84dacfce0e8e00acabe34fba164dbbd409e
DylanAdam <adamdylan.p@gmail.com>
2019-02-19 17:06:51 +0100

    fixed sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,4 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :experiences
   has_many :bookings
-  validates :company, :first_name, :last_name, presence: true
 end
