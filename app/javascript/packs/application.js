d1a9610c0df116a4dbbfe8d04fef54da17fc020d
Edouard Vaudour <edouard.vaudour@icloud.com>
2019-02-20 10:49:53 +0100

    Addmap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,9 @@ $(function(){
      $(this).addClass("active");
   });
 });
+
+import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+
+import { initMapbox } from '../plugins/init_mapbox';
+
+initMapbox();
