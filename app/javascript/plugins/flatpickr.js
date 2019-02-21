3a154c75971b628d292224a64b48b580518c1d10
claire demont <claire.edith.demont@gmail.com>
2019-02-21 10:57:48 +0100

    add calendar to new form

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,5 +1,5 @@
 import flatpickr from "flatpickr"
-import "flatpickr/dist/themes/airbnb.css" // Note this is important!
+import "flatpickr/dist/themes/material_blue.css" // Note this is important!
 
 flatpickr(".datepicker", {
   altInput: true,
