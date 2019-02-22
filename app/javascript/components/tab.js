cac12e3a8ad59f797e4c50729e8f834cfa63b3fc
Edouard Vaudour <edouard.vaudour@icloud.com>
2019-02-22 13:03:15 +0100

    Photo

--- app/javascript/components/tab.js
+++ app/javascript/components/tab.js
@@ -5,20 +5,24 @@ const selectTab = () => {
   const bookingsAsOwnerHead = document.querySelector(".tabs .bookings-as-owner-head");
   const mybookingsHead = document.querySelector(".tabs .my-bookings-head");
 
-  mybookingsHead.addEventListener("click", (event) => {
-    console.log(event);
-    event.currentTarget.classList.add("active");
-    bookingsAsOwnerHead.classList.remove("active");
-    mybookings.classList.remove("hidden");
-    bookingsAsOwner.classList.add("hidden");
-  });
+  if (mybookingsHead) {
+    mybookingsHead.addEventListener("click", (event) => {
+      console.log(event);
+      event.currentTarget.classList.add("active");
+      bookingsAsOwnerHead.classList.remove("active");
+      mybookings.classList.remove("hidden");
+      bookingsAsOwner.classList.add("hidden");
+    });
+  }
 
-  bookingsAsOwnerHead.addEventListener("click", (event) => {
-    event.currentTarget.classList.add("active");
-    mybookingsHead.classList.remove("active");
-    mybookings.classList.add("hidden");
-    bookingsAsOwner.classList.remove("hidden");
-  });
+  if (bookingsAsOwnerHead) {
+    bookingsAsOwnerHead.addEventListener("click", (event) => {
+      event.currentTarget.classList.add("active");
+      mybookingsHead.classList.remove("active");
+      mybookings.classList.add("hidden");
+      bookingsAsOwner.classList.remove("hidden");
+    });
+  }
 }
 
 export { selectTab };
