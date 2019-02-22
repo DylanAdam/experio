8f3b4bf0e60b8a1ffe85d8422fa32de8dcb3c7c2
claire demont <claire.edith.demont@gmail.com>
2019-02-22 11:01:30 +0100

    booking index ok

--- app/javascript/components/status.js
+++ app/javascript/components/status.js
@@ -1,18 +1,18 @@
-const changeStatus = () => {
+// const changeStatus = () => {
 
-  const acceptBtn = document.querySelectorAll(".accept");
-  const declineBtn = document.querySelectorAll(".decline");
+//   const acceptBtn = document.querySelectorAll(".accept");
+//   const declineBtn = document.querySelectorAll(".decline");
 
-acceptBtn.forEach((element) => {
- element.addEventListener("click", (event) => {
-  const idBooking = event.currentTarget.dataset.id;
+// acceptBtn.forEach((element) => {
+//  element.addEventListener("click", (event) => {
+//   const idBooking = event.currentTarget.dataset.id;
 
-// trouver le booking avec l'id
-  const booking = ;
-  console.log(idBooking)
-  booking.status = "accepted"
-  });
- });
-};
+// // trouver le booking avec l'id
+//   const booking = ;
+//   console.log(idBooking)
+//   booking.status = "accepted"
+//   });
+//  });
+// };
 
-export { changeStatus };
+// export { changeStatus };
