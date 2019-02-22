6d8f5e82614a3ad06b5eeb4bd1db66d45dee0c8b
alexandre cousin <cousin.a@laposte.net>
2019-02-22 10:35:13 +0100

    map sticky

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -7,7 +7,7 @@ const buildMap = () => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   return new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10'
+    style: 'mapbox://styles/alecou/cjseud9mo0qfp1fqydsglpc97'
   });
 };
 
