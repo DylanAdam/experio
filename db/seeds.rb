88db3d92913d23744529bbc1e1f4a78bf81a1bf1
Edouard Vaudour <edouard.vaudour@icloud.com>
2019-02-22 15:33:22 +0100

    Alex photo

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,32 +7,31 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
-user1 = User.create!(email: "edouard.vaudour@icloud.com", password: "azerty", first_name: "Edouard", last_name: "Vaudour", company: "Luma")
+user1 = User.create!(email: "alex@yahoo.fr", password: "azerty", first_name: "Alex", last_name: "Cousin", company: "Tesla")
 user2 = User.create!(email: "adamdylan.p@gmail.com", password: "azerty", first_name: "Dylan", last_name: "Adam", company: "Space x")
-user3 = User.create!(email: "claire.edith.demont@gmail.com", password: "azerty", first_name: "Claire", last_name: "Demont", company: "Monsanto")
-user4 = User.create!(email: "alex@yahoo.fr", password: "azerty", first_name: "Alex", last_name: "Cousin", company: "Tesla")
 
-puts "users created"
+# user3 = User.create!(email: "claire.edith.demont@gmail.com", password: "azerty", first_name: "Claire", last_name: "Demont", company: "Monsanto")
 
-titles = ["Escape Game", "Laser game", "Bowling", "VR laser game", "Paintball", "Wine tasting", "Boat Party"]
-9.times do
-  description = Faker::TvShows::HowIMetYourMother.quote
-  Experience.create!(title: titles.sample, description: description, address:Faker::Address.street_address, price: Faker::Commerce.price, capacity_min: rand(4..12), validity_date: Faker::Date.forward(30), category: ["Indoor", "Outdoor", "Culture", "Fun", "Extreme"].sample, photo: "https://cap-evenement.fr/wp-content/uploads/2017/02/organiser-un-team-building-a-mulhouse-1.jpg", user: User.all.sample, duration: ["3h", "2h", "2h30", "1h", "1h30", "50min"].sample)
-end
+# puts "users created"
 
-puts "9 experiences created"
+# titles = ["Escape Game", "Laser game", "Bowling", "VR laser game", "Paintball", "Wine tasting", "Boat Party"]
+# 9.times do
+#   description = Faker::TvShows::HowIMetYourMother.quote
+#   Experience.create!(title: titles.sample, description: description, address:Faker::Address.street_address, price: Faker::Commerce.price, capacity_min: rand(4..12), validity_date: Faker::Date.forward(30), category: ["Indoor", "Outdoor", "Culture", "Fun", "Extreme"].sample, photo: "https://cap-evenement.fr/wp-content/uploads/2017/02/organiser-un-team-building-a-mulhouse-1.jpg", user: User.all.sample, duration: ["3h", "2h", "2h30", "1h", "1h30", "50min"].sample)
+# end
 
-@description = "Vagabund Brauerei not only wants to make beer for people to drink, but we also want to get people involved in the beer making process. That's why we offer brew courses to beginners, who want to have a better handle on how beer is made. The beginner's course is also good for homebrewers who want to have a look at what goes into brewing on a small industrial sized brew system. A course consists of a small group of people in order to make it more personal. We get started at 10:30 and go until 17:00. The course includes coffee, beers, snacks, lunch, and a free glass of the finished beer once the beer is ready to be served. Participation costs €125 (including tax) per person. If interested, please send an email to info@vagabundbrauerei.com."
-Experience.create!(title: "Beer brewing workshop", description: @description, address:"5 rue du sentier, 75002 PARIS", photo: "http://static.giantbomb.com/uploads/original/9/99864/2419866-nes_console_set.png", price: 40, capacity_min: 5, validity_date: Faker::Date.forward(30), category: "Indoor", user: User.all.sample, duration: "3h")
+# puts "9 experiences created"
 
+# @description = "Vagabund Brauerei not only wants to make beer for people to drink, but we also want to get people involved in the beer making process. That's why we offer brew courses to beginners, who want to have a better handle on how beer is made. The beginner's course is also good for homebrewers who want to have a look at what goes into brewing on a small industrial sized brew system. A course consists of a small group of people in order to make it more personal. We get started at 10:30 and go until 17:00. The course includes coffee, beers, snacks, lunch, and a free glass of the finished beer once the beer is ready to be served. Participation costs €125 (including tax) per person. If interested, please send an email to info@vagabundbrauerei.com."
+# Experience.create!(title: "Beer brewing workshop", description: @description, address:"5 rue du sentier, 75002 PARIS", photo: "http://static.giantbomb.com/uploads/original/9/99864/2419866-nes_console_set.png", price: 40, capacity_min: 5, validity_date: Faker::Date.forward(30), category: "Indoor", user: User.all.sample, duration: "3h")
 
-booking1 = Booking.create!(date: Date.today, number_of_participants: rand(1..15), user: user1, experience: Experience.all.sample)
-booking2 = Booking.create!(date: Date.today, number_of_participants: rand(1..15), user: user2, experience: Experience.all.sample)
-booking3 = Booking.create!(date: Date.today, number_of_participants: rand(1..15), user: user3, experience: Experience.all.sample)
+# booking1 = Booking.create!(date: Date.today, number_of_participants: rand(1..15), user: user1, experience: Experience.all.sample)
+# booking2 = Booking.create!(date: Date.today, number_of_participants: rand(1..15), user: user2, experience: Experience.all.sample)
+# booking3 = Booking.create!(date: Date.today, number_of_participants: rand(1..15), user: user3, experience: Experience.all.sample)
 
-review1 = Review.create!(description: Faker::Movies::StarWars.quote, rating: rand(0..5), booking: booking1)
-review2 = Review.create!(description: Faker::Movies::StarWars.quote, rating: rand(0..5), booking: booking2)
-review3 = Review.create!(description: Faker::Movies::StarWars.quote, rating: rand(0..5), booking: booking3)
+# review1 = Review.create!(description: Faker::Movies::StarWars.quote, rating: rand(0..5), booking: booking1)
+# review2 = Review.create!(description: Faker::Movies::StarWars.quote, rating: rand(0..5), booking: booking2)
+# review3 = Review.create!(description: Faker::Movies::StarWars.quote, rating: rand(0..5), booking: booking3)
 
 # # Testing Photos Upload
 # @url = "http://static.giantbomb.com/uploads/original/9/99864/2419866-nes_console_set.png"
